69c4e7af023d6d1f2b4fa8224568f81a8dfb22ad
超级无敌哥布林大王 <15176388+super-invincible-goblin-king@user.noreply.gitee.com>
2025-11-17 21:37:05 +0800

    dkd合作商管理代码改造

--- dkd-manage/src/main/java/com/dkd/manage/controller/PartnerController.java
+++ dkd-manage/src/main/java/com/dkd/manage/controller/PartnerController.java
@@ -2,6 +2,9 @@ package com.dkd.manage.controller;
 
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
+
+import com.dkd.common.utils.SecurityUtils;
+import com.dkd.manage.domain.vo.PartnerVo;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,7 +45,7 @@ public class PartnerController extends BaseController
     public TableDataInfo list(Partner partner)
     {
         startPage();
-        List<Partner> list = partnerService.selectPartnerList(partner);
+        List<PartnerVo> list = partnerService.selectPartnerVoList(partner);
         return getDataTable(list);
     }
 
@@ -101,4 +104,22 @@ public class PartnerController extends BaseController
     {
         return toAjax(partnerService.deletePartnerByIds(ids));
     }
+
+    /**
+     * 合作商重置密码
+     * @return AjaxResult
+     */
+    @PreAuthorize("@ss.hasPermi('manage:partner:edit')")
+    @Log(title = "重置合作商密码", businessType = BusinessType.UPDATE)
+    @PutMapping("/resetPwd/{id}")
+    public AjaxResult resetPassword(@PathVariable Long id) {//1.接收参数
+        //2.创建合作商对象
+        Partner partner = new Partner();
+        partner.setId(id);
+        partner.setPassword(SecurityUtils.encryptPassword("123456"));
+        //3.更新合作商密码
+        return toAjax(partnerService.updatePartner(partner));
+    }
+
+
 }
